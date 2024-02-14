4dcf217bee4fde25c3384eafe3ed7c1c35b1306a
Mohit <Harshitsri1625@gmail.com>
2024-02-14 11:26:03 +0530

    Initial

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+// @ts-check
+
+import Header from "@/components/shared/Header.tsx";
+import Footer from "@/components/shared/Footer.tsx";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
